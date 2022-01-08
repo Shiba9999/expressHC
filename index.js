a0ab93d893c91179fdd897ec75e23a9ce8c895f7
shiba <rp.shiba2@gmail.com>
2022-01-09 01:46:04 +0530

    setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+var format = require("date-format");
+
+const PORT = 4000 || process.env.PORT;
+app.get("/", (req, res) => {
+  res.status(400).send("good res");
+});
+app.get("/api/v1/instagram", (req, res) => {
+  const instaSocial = {
+    name: "shiba",
+    followers: "500",
+    follows: "50",
+    date: format.asString("hh:mm:ss.SSS", new Date()),
+  };
+  res.json(instaSocial);
+});
+app.get("/api/v1/:name/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(req.params.name);
+});
+app.listen(PORT, () => {
+  console.log(`running at ${PORT}`);
+});
